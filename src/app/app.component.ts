822ae40cc48f3b1721d2ffa94e604dd8fb3e3ac0
klumba12 <dimitry.sitnov@gmail.com>
2018-04-24 18:41:25 +0300

    apply styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { ConditionBuilderModel } from '../condition-builder/condition-builder.mo
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
