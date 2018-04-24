3db67567ee1d065434a364397f8abac80a8f97d9
klumba12 <dimitry.sitnov@gmail.com>
2018-04-24 16:27:46 +0300

    show browser animation modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { ThemeModule } from '../theme/material/theme.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ConditionBuilderModule,
     ThemeModule
   ],
--- src/theme/material/condition-builder-tpl.component.ts
+++ src/theme/material/condition-builder-tpl.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, ViewEncapsulation } from "@angular/core";
 
 @Component({
     selector: 'q-condition-builder-tpl',
     templateUrl: 'condition-builder-tpl.component.html',
-    styleUrls: ['./condition-builder-tpl.component.scss']
+    styleUrls: ['./condition-builder-tpl.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class ConditionBuilderTplComponent {
 }
