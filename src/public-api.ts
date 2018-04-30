3a16db82698cd2988c94498419ec678a4a6e5b16
klumba12 <dimitry.sitnov@gmail.com>
2018-04-30 10:27:53 +0300

    fix aot

---
+++ src/public-api.ts
@@ -0,0 +1,4 @@
+export * from './condition-builder/condition-builder.module';
+export * from './condition-builder/condition-builder.model';
+export * from './theme/material/theme.module';
+export * from './expression-builder/eb.module';
