15c456a5d013a6e69418ef96b2151f920da90be4
klumba12 <dimitry.sitnov@gmail.com>
2019-04-16 12:58:42 +0300

    upd settings. json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { ISerializationNode } from '../expression-builder/serialization.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   @ViewChild(ConditionBuilderComponent)
